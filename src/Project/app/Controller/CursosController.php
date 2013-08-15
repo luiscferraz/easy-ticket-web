28ed93d414ef6de90951345b0ead8d61fb854144
nandacms@gmail.com <nandacms@gmail.com@49ebcc9c-c244-8906-279a-3788848860ac>
2013-08-15 03:27:46 +0000

    Alteração no CursosController para o método add.

--- src/Project/app/Controller/CursosController.php
+++ src/Project/app/Controller/CursosController.php
@@ -5,9 +5,18 @@
  	public function index(){
 	    //Pega todos os elementos curso e retorna na view
 	    $this->set('title_for_layout', 'Curso');
- 		$this -> layout = 'IndexPessoa';
+ 		$this -> layout = 'IndexCurso';
  		$this -> set ('cursos', $this-> Curso->find('all'));
  	}
+
+ 	function add() {
+        if (!empty($this->data)) {
+            if ($this->Curso->save($this->data)) {
+                $this->Session->setFlash('O curso foi cadastrado com sucesso!');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
  	
  }
  	
