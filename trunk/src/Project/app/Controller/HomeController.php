47b2b0b7961fb5a0313eff1c8a17a287cc1783db
allandoamaral@gmail.com <allandoamaral@gmail.com@49ebcc9c-c244-8906-279a-3788848860ac>
2013-08-24 01:06:23 +0000

    Alterações Home

--- trunk/src/Project/app/Controller/HomeController.php
+++ trunk/src/Project/app/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
  class HomeController extends AppController{
         public function index () {
+        		$this->set('title_for_layout', 'EasyTicket Web');
                $this->layout =  'base'; 
         }
  }
