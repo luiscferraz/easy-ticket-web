2a6065a14dc76317ec396ce1cfa34487b112a469
allandoamaral@gmail.com <allandoamaral@gmail.com@49ebcc9c-c244-8906-279a-3788848860ac>
2013-08-31 03:45:22 +0000

    CRUD Roles

---
+++ src/Project/app/Controller/RolesController.php
@@ -0,0 +1,93 @@
+<?php
+
+ class RolesController extends AppController {
+    public $uses = array ('Role');
+
+ 	  public function index(){
+        $this->set('title_for_layout', 'Cargos');
+        $this -> layout = 'index';
+
+        $roles = $this->Role->find('all');
+        $this -> set ('roles', $roles);
+        }
+
+ 	public function add() {
+        $this->set('title_for_layout', 'Cadastrar Cargo');
+        $this->layout = 'base';
+        if (!empty($this->data)) {
+            if($this->request->is('post')){
+                if ($this -> verifica($this->request->data)) {
+                    if($this->Role->saveAll($this->request->data)){
+                        $this->Session->setFlash('O cargo foi adicionado com sucesso.');
+                        $this->redirect(array('action' => 'index'));
+                    }
+                    else{
+                        $this->Session->setFlash($this->flashError('Erro ao cadastrar cargo!'));
+                    }       
+                }       
+            }
+            else{
+                $this->Session->setFlash($this->Session->setFlash($this->flashError('O cargo não foi adicionado. Tente novamente!')));          
+            }   
+        }
+        
+    }
+ 	
+    public function verifica($data) {
+        #echo $data['Role']['name'];
+        $ctr = 0;
+        $strerro = '';
+
+        //Aluno existente
+        $ext = $this -> Role -> query ( "SELECT * FROM `roles` WHERE name = '". $data['Role']['name']."'" );
+        #var_export($ext);
+        if (!empty($ext)){
+            $ctr ++;
+            $strerro = $strerro . 'Cargo já cadastrado.';
+        }
+
+        if ($ctr > 0) {
+            #procurar para definicao de erro
+            #$this -> Session -> setFlash ($this -> flashError ($strerro));
+            echo 'Cargo já cadastrado';
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public function edit($id = NULL){
+        $this->set('title_for_layout', 'Editar Cargo');
+        $this->layout = 'base';
+        $this->Role->id = $id;
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+    
+        $role = $this->Role->findById($id);
+        if (!$role) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        if ($this->request->is('get')) {
+            $this->request->data = $this->Role->read();
+        } 
+        else {
+            $this->Role->id = $id;
+            if ($this->Role->saveAll($this->request->data)) {
+                
+                $this->Session->setFlash('Os dados do cargo foram editados!');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+   }
+
+    public function delete($id) {
+        $this->Role->delete($id);
+        $this->Session->setFlash('O cargo foi deletado!');
+        $this->redirect(array('action'=>'index'));
+    }
+
+ }
+ 	
+?>
---
+++ src/Project/app/View/Employees/edit.ctp
@@ -0,0 +1,31 @@
+<?php 
+		echo $this->Form->create('Employee', array('action' => 'edit')); ?>
+
+
+		<div class="left">
+				
+			<fieldset id="dados_aluno1">
+				<legend class="legenda">Dados do Funcionário</legend>		
+
+						<?php echo $this->Form->input('Employee.name', array('label' => 'Nome: ','required'=>'required', 'id'=>'name')); ?> <br>
+						<?php echo $this->Form->input('Employee.cpf', array('label' => 'CPF: ','required'=>'required', 'id'=>'cpf'));?> <br>
+						<?php echo $this->Form->input('Employee.birthday', array('label' => 'Data de Nascimento: ','required'=>'required', 'id'=>'birthday'));?> <br>
+						<?php echo $this->Form->input('Employee.phone', array('label' => 'Telefone: ','required'=>'required', 'id'=>'phone'));?> <br>
+						<?php echo $this->Form->input('Employee.email', array('label' => 'E-mail: ','required'=>'required', 'id'=>'email'));?> <br> 
+				
+			</fieldset>
+
+			<fieldset id="dados_aluno2">						
+						
+						<?php echo $this->Form->input('Employee.login', array('label' => 'Login: ','required'=>'required', 'id'=>'login'));?> <br>
+						<?php echo $this->Form->input('Employee.password', array('label' => 'Senha: ', 'type'=>'password','required'=>'required', 'id'=>'password'));?> <br>
+						<?php echo $this->Form->input('Employee.status', array('options' => array("ATIVO", "INATIVO"), 'empty' => 'Selecione', 'type'=>'select','label' => 'Status: ', 'id'=>'status')); ?> <br>
+						<?php echo $this->Form->input('Employee.role', array('label' => 'Cargo: ','required'=>'required', 'id'=>'role'));?> <br>
+					
+			</fieldset>
+
+			<div id="botaoCadastrar"> 
+				<?php echo $this->Form->end('Cadastrar Funcionário', array('id'=>'button')); ?> 
+			</div>
+		
+		</div>
