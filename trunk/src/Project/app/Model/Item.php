aa6bc7fe3b0b5921bde7d1dffedc110d92df9263
marcela.domingues@gmail.com <marcela.domingues@gmail.com@49ebcc9c-c244-8906-279a-3788848860ac>
2013-09-06 02:35:01 +0000

    Validação de cpf.

--- trunk/src/Project/app/Model/Item.php
+++ trunk/src/Project/app/Model/Item.php
@@ -1,5 +1,8 @@
 <?php
 Class Item extends AppModel{
 	public $name='Item';
+		public $hasAndBelongsToMany = array('Menu' => array('className'=>'Menu', 'joinTable'=>'items_menus',
+		'foreignKey'=>'item_id', 'associationForeignKey'=>'menu_id',));
+
 }
 ?>
--- trunk/src/Project/app/Model/Menu.php
+++ trunk/src/Project/app/Model/Menu.php
@@ -1,5 +1,9 @@
 <?php
 Class Menu extends AppModel{
 	public $name='Menu';
+	public $hasAndBelongsToMany = array('Item' => array('className'=>'Item', 'joinTable'=>'items_menus',
+		'foreignKey'=>'menu_id', 'associationForeignKey'=>'item_id',));
+
+
 }
 ?>
